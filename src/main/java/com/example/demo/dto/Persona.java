50de6a942ef9665ea77a43feed200ef4a2756be9
Sharon Linares <akiralyn02@gmail.com>
2025-02-28 12:08:04 +0100

    cree la clase persona y producto

--- src/main/java/com/example/demo/dto/Persona.java
+++ src/main/java/com/example/demo/dto/Persona.java
@@ -2,4 +2,9 @@ package com.example.demo.dto;
 
 public class Persona {
 
+	private String nombre = "Sharon";
+	private int edad = 22;
+	private double altura = 1.60;
+	
+	
 }
--- src/main/java/com/example/demo/dto/Producto.java
+++ src/main/java/com/example/demo/dto/Producto.java
@@ -1,5 +1,12 @@
 package com.example.demo.dto;
 
 public class Producto {
+	
+	
+	private String nombre = "Laptop"
+	private int cantidad = 23;
+	private double precio = 203.345;
+	
+	
 
 }
