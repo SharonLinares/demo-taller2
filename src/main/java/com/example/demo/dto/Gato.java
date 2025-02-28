4fb5608895ad7315b506fecb8e173c507fbadd14
Sharon Linares <akiralyn02@gmail.com>
2025-02-28 11:37:23 +0100

    cree la clase perro y gato

---
+++ src/main/java/com/example/demo/dto/Gato.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+public class Gato {
+
+	private String raza = "vaquita";
+	private int edad = 2;
+	private double peso = 4.5;
+	
+}
---
+++ src/main/java/com/example/demo/dto/Perro.java
@@ -0,0 +1,8 @@
+package com.example.demo.dto;
+
+public class Perro {
+	
+	private String raza = "canchozo";
+	private int edad = 2;
+	private double peso = 4.5;
+}
