a1db42fc6bb16d506d054351e7542142af9e5445
Sharon Linares <akiralyn02@gmail.com>
2025-02-28 11:50:46 +0100

    cree la clase leon y coche

---
+++ src/main/java/com/example/demo/dto/Coche.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+public class Coche {
+
+	private String marca = "Toyota";
+	private int modelo = 145;
+	private double kilometraje = 254.234;
+	
+}
---
+++ src/main/java/com/example/demo/dto/Leon.java
@@ -0,0 +1,8 @@
+package com.example.demo.dto;
+
+public class Leon {
+
+	private String nombre = "mufasa";
+	private int edad = 3;
+	private double peso = 235.2;
+}
