cb6b4b9a6d4d3837358fe95695c5033124879cb3
Sharon Linares <akiralyn02@gmail.com>
2025-02-28 11:57:13 +0100

    cree la clase barco y bicicleta

---
+++ src/main/java/com/example/demo/dto/Barco.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+public class Barco {
+	
+	private String marca = "bavaria";
+	private int modelo = 278;
+	private double altura = 23.4;
+	
+	
+
+}
---
+++ src/main/java/com/example/demo/dto/Bicicleta.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+public class Bicicleta {
+
+	private String tipo = "Montaña";
+	private int modelo = 235;
+	private double peso = 5.2;
+	
+	
+	
+}
