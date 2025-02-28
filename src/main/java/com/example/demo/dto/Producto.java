6d91eec44a50c731931c55c9d16588680f1a19a5
Sharon Linares <akiralyn02@gmail.com>
2025-02-28 12:09:11 +0100

    cree la clase persona y producto

--- src/main/java/com/example/demo/dto/Producto.java
+++ src/main/java/com/example/demo/dto/Producto.java
@@ -3,7 +3,7 @@ package com.example.demo.dto;
 public class Producto {
 	
 	
-	private String nombre = "Laptop"
+	private String nombre = "Laptop";
 	private int cantidad = 23;
 	private double precio = 203.345;
 	
